fcdab3b3b0367359ddf16ee09e46650e21b6d031
Osama Azhar <osamaazhar591@gmail.com>
2025-11-25 17:22:38 +0400

    Add circuit breaker configuration for payment and product services

--- src/main/java/com/oasys/common_module/clients/external/PaymentClient.java
+++ src/main/java/com/oasys/common_module/clients/external/PaymentClient.java
@@ -5,10 +5,13 @@ package com.oasys.common_module.clients.external;
 import com.oasys.common_module.clients.config.FeignCommonConfig;
 import com.oasys.common_module.clients.external.model.PaymentRequest;
 import com.oasys.common_module.clients.external.model.PaymentResponse;
+import com.oasys.common_module.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CircuitBreaker(name = "paymentServiceCircuitBreaker", fallbackMethod = "paymentServiceFallback")
 @FeignClient(name = "PAYMENT-SERVICE", path = "/payment", configuration = FeignCommonConfig.class)
 public interface PaymentClient {
 
@@ -17,4 +20,9 @@ public interface PaymentClient {
 
     @GetMapping("/response/{orderId}")
     ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable("orderId") long orderId);
+
+    default void paymentServiceFallback(Exception exception) {
+        throw new CustomException("Payment Service is currently unavailable. Please try again later."
+                , "SERVICE UNAVAILABLE", 500);
+    }
 }
--- src/main/java/com/oasys/common_module/clients/external/ProductClient.java
+++ src/main/java/com/oasys/common_module/clients/external/ProductClient.java
@@ -5,10 +5,14 @@ package com.oasys.common_module.clients.external;
 import com.oasys.common_module.clients.config.FeignCommonConfig;
 import com.oasys.common_module.clients.external.model.ProductRequest;
 import com.oasys.common_module.clients.external.model.ProductResponse;
+import com.oasys.common_module.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CircuitBreaker(name = "productServiceCircuitBreaker", fallbackMethod = "productServiceFallback")
 @FeignClient(name = "PRODUCT-SERVICE", path = "/product", configuration = FeignCommonConfig.class)
 public interface ProductClient {
 
@@ -20,4 +24,14 @@ public interface ProductClient {
 
     @PutMapping("/reduceQuantity/{id}")
     ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity);
+
+
+    // Fallbacks for circuit breaker (overloaded to match each method signature + Throwable)
+
+    default void productServiceFallback(Exception exception) {
+      throw new CustomException("Product Service is currently unavailable. Please try again later."
+      , "SERVICE UNAVAILABLE", 500);
+    }
+
+
 }
