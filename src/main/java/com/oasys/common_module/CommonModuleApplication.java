27afca3fa30c180085c1502dbcb0a4f409c4ee5a
Osama Azhar <osamaazhar591@gmail.com>
2025-11-30 19:47:56 +0400

    Add OAuth2 security configuration and request interceptor for internal client

--- src/main/java/com/oasys/common_module/CommonModuleApplication.java
+++ src/main/java/com/oasys/common_module/CommonModuleApplication.java
@@ -3,13 +3,44 @@ package com.oasys.common_module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.oasys.common.clients.external")
 public class CommonModuleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommonModuleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommonModuleApplication.class, args);
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository
+    ) {
+
+        OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider
+                = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+
+        DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager
+                = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository,
+                authorizedClientRepository
+        );
+
+        oAuth2AuthorizedClientManager.setAuthorizedClientProvider(oAuth2AuthorizedClientProvider);
+
+        return oAuth2AuthorizedClientManager;
+    }
 
 }
